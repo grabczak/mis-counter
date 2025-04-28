24bbfde9cbdbbe764fa7edc3acd35365a1a5218e
Kacper Grabczak <kacpergrabczak@gmail.com>
2025-04-28 23:50:29 +0200

    Add mis_count method

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,152 @@
 use std::fs::File;
 use std::io::{self, BufRead};
+use std::collections::HashMap;
 
-fn read_file(filename: &str) -> io::Result<Vec<Vec<i32>>> {
-    let file = File::open(filename)?;
-    let reader = io::BufReader::new(file);
+type Value = i32;
+type Children = Vec<Value>;
+type Nodes = HashMap<Value, Children>;
 
-    let mut data: Vec<Vec<i32>> = Vec::new();
-
-    for line_result in reader.lines() {
-        let line = line_result?;
-        let numbers: Vec<i32> = line
-            .split(' ')
-            .filter_map(|s| s.trim().parse::<i32>().ok())
-            .collect();
-        data.push(numbers);
-    }
-
-    Ok(data)
+pub struct Tree {
+    root: Value,
+    nodes: Nodes,
 }
 
-fn print_tree(data: &[Vec<i32>]) {
-    println!("Tree adjacency list:");
-    for row in data {
-        for val in row {
-            print!("{} ", val);
+impl Tree {
+    fn new(root: Value) -> Self {
+        Tree {
+            root,
+            nodes: HashMap::new(),
         }
-        println!();
+    }
+
+    fn insert(&mut self, node: Value, children: Children) {
+        self.nodes.insert(node, children);
+    }
+
+    // fn print(&self) {
+    //     for (value, children) in &self.nodes {
+    //         print!("{} ", value);
+    //         for child in children {
+    //             print!("{} ", child);
+    //         }
+    //         println!("")
+    //     }
+    // }
+
+    fn build(&mut self, filename: &str) -> io::Result<()> {
+        let file = File::open(filename)?;
+        let reader = io::BufReader::new(file);
+    
+        let mut lines: Vec<Vec<i32>> = Vec::new();
+    
+        for line_result in reader.lines() {
+            let line = line_result?;
+            let numbers: Vec<i32> = line
+                .split(' ')
+                .filter_map(|s| s.trim().parse::<i32>().ok())
+                .collect();
+            lines.push(numbers);
+        }
+
+        for line in lines {
+            match line.split_first() {
+                Some((value, children)) => self.insert(*value, children.to_vec()),
+                None => ()
+            }
+        }
+
+        Ok(())
+    }
+
+    fn children(&self, node: Value) -> Vec<Value> {
+        self.nodes.get(&node).cloned().unwrap_or_else(Vec::new)
+    }
+
+    fn grandchildren(&self, node: Value) -> Vec<Value> {
+        let mut result = Vec::new();
+
+        if let Some(children) = self.nodes.get(&node) {
+            for &child in children {
+                if let Some(grandchildren) = self.nodes.get(&child) {
+                    result.extend(grandchildren.iter().cloned());
+                }
+            }
+        }
+
+        result
+    }
+
+    fn post_order(&self) -> Vec<Value> {
+        fn _order(tree: &Tree, node: Value, result: &mut Vec<Value>) {
+            if let Some(children) = tree.nodes.get(&node) {
+                for &child in children {
+                    _order(tree, child, result);
+                }
+            }
+
+            result.push(node);
+        }
+
+        let mut result = Vec::new();
+
+        _order(self, self.root, &mut result);
+
+        result
+    }
+
+    fn mis_count(&self) -> i32 {
+        let mut mu: HashMap<Value, i32> = HashMap::new();
+        let mut nu: HashMap<Value, i32> = HashMap::new();
+
+        let post_order_nodes = self.post_order();
+
+        for node in post_order_nodes {
+            let mut m = 1;
+            let mut n = 1;
+
+            for child in self.children(node) {
+                m = match mu.get(&child) {
+                    Some(value) => m * value,
+                    None => 0
+                };
+
+                n = match nu.get(&child) {
+                    Some(value) => n * value,
+                    None => 0
+                };
+            }
+
+            nu.insert(node, m - n);
+
+            let mut k = 1;
+
+            for grandchild in self.grandchildren(node) {
+                k = match mu.get(&grandchild) {
+                    Some(value) => k * value,
+                    None => 0
+                };
+            }
+
+            k = match nu.get(&node) {
+                Some(value) => k + value,
+                None => 0
+            };
+
+            mu.insert(node, k);
+        }
+
+        match mu.get(&self.root) {
+            Some(value) => *value,
+            None => 0
+        }
     }
 }
 
 fn main() {
-    println!("Enter the CSV file name:");
-    let mut filename = String::new();
-    io::stdin()
-        .read_line(&mut filename)
-        .expect("Failed to read input");
+    let mut tree = Tree::new(0);
 
-    let filename = filename.trim();
-
-    match read_file(filename) {
-        Ok(data) => print_tree(&data),
-        Err(e) => eprintln!("Error reading file: {}", e),
+    match tree.build("example.csv") {
+        Ok(_) => println!("Mis count: {}", tree.mis_count()),
+        Err(e) => eprintln!("Error reading file: {}", e)
     }
 }
